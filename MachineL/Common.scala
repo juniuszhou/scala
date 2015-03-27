3f750e6e725bd2692cca84d3d5fe85207b941f36
juniuszhou <junius.zhou@gmail.com>
2015-03-27 08:28:51 +0800

    add MachineL

---
+++ MachineL/Common.scala
@@ -0,0 +1,32 @@
+package MachineL
+
+/**
+ * Created by juzhou on 1/26/2015.
+ */
+
+class OutputClass{
+
+}
+class InputFeature{
+
+}
+
+class InputData{
+
+}
+
+class TrainData{
+
+}
+
+
+class Common {
+
+}
+
+object Common{
+  def getFeatureByName(str: String) = {
+
+  }
+   println("hello")
+}
---
+++ MachineL/NavieBayes.scala
@@ -0,0 +1,20 @@
+package MachineL
+
+/**
+ * Created by juzhou on 1/26/2015.
+ */
+class NavieBayesModel{
+  def train(): Unit = {
+
+  }
+
+  def predict(): OutputClass = {
+    val res = new OutputClass
+    res
+  }
+}
+object NavieBayes {
+  def main (args: Array[String]) {
+
+  }
+}
